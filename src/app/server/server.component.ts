6140b1b2fb51f72a647160386938ba498a4d0be1
haleykelling <haley@kellings.com>
2020-09-15 16:46:17 -0600

    Add button with click event listener.

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class ServerComponent {
     serverId = 10;
     serverStatus: string = 'offline';
+
+    getServerStatus() {
+        return this.serverStatus
+    }
 }
